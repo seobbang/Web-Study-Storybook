8e7427d52471477f63b9f59e7174b2648afa08d8
seobbang <seobbangway@naver.com>
2023-06-02 01:20:48 +0900

    feat: styled-component 적용 예시

--- taskbox/.storybook/preview.js
+++ taskbox/.storybook/preview.js
@@ -1,4 +1,17 @@
 /** @type { import('@storybook/react').Preview } */
+// import { ThemeProvider } from "styled-components";
+// import GlobalStyle from "src/styles/GlobalStyle";
+
+// 어떤 스토리가 선택되었든 간에 GlobalStyle이 적용되도록 해줌!
+// export const decorators = [
+//   (Story) => (
+//     <ThemeProvider theme="default">
+//       <GlobalStyle />
+//       <Story />
+//     </ThemeProvider>
+//   ),
+// ];
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
